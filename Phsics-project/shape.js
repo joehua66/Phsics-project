0ce4359e08d1bc658511e3e9043d8cb8327d0ade
joehua66 <157079933+joehua66@users.noreply.github.com>
2024-03-28 02:15:31 -0400

    Add files via upload

--- Phsics-project/shape.js
+++ Phsics-project/shape.js
@@ -6,7 +6,6 @@ class Shape {
     this.pos = pos;
     this.size = size;
     this.body = this.createBody(pos, options);
-    
     Matter.Composite.add(engine.world, this.body);
   }
 
@@ -16,6 +15,19 @@ class Shape {
 
   display() {
     
+  }
+
+  isDead() {
+    if (this.body.position.y > height + 100) {
+      Matter.Composite.remove(this.world, this.body);
+      return true;
+    }
+    return false;
+  }
+
+  animAngle(targetAngle) {
+    let angle = lerp(this.body.angle, targetAngle, 0.3);
+    Matter.Body.setAngle(this.body, angle);
   }
 
 
--- Phsics-project/sketch.js
+++ Phsics-project/sketch.js
@@ -1,19 +1,58 @@
 let engine;
 let shapes = [];
+let walls;
+let mouse;
+//let leftPaddle;
+//let rightPaddle;
 
 function setup() {
   createCanvas(600, 600);
   rectMode(CENTER);
-
   engine = Matter.Engine.create();
+  walls = new Walls(engine.world);
+  walls.addSideWalls();
+  walls.addBottomWall();
 
-  const ground = new Rect(engine.world, 
-    createVector(width/2, height),
-    createVector(width - 10, 30), { isStatic: true });
+  /*leftPaddle = new Rect(engine.world, 
+    createVector(width * 0.3, height * 0.8),
+    createVector(width * 0.3, 30), 
+    {isStatic:true, angle: PI/4});
+
+  rightPaddle = new Rect(engine.world, 
+    createVector(width * 0.7, height * 0.8),
+    createVector(width * 0.3, 30), 
+    {isStatic:true, angle: -PI/4});
+  shapes.push(leftPaddle, rightPaddle);*/
   
   Matter.Runner.run(engine);
-  shapes.push(ground);
+
+  setInterval(() => {
+    if (shapes.length < 1) {
+      const options = {
+        friction: 0, 
+        frictionAir: 0,
+        restitution: 1, 
+        inertia: Infinity
+      }
+      this.createCircle(
+        10, height-10, options);
+      this.createShape(random(width*0.75,width),height/2,null)
+      
+    }
+  }, 1000);
 }
+
+
+
+function createCircle(x, y, options) {
+  let circle = new Circle(engine.world,
+      createVector(x, y), 
+      createVector(25, 25),
+      options);
+  
+  shapes.push(circle);
+}
+
 
 function createShape(x, y, options) {
   let shape;
@@ -31,14 +70,33 @@ function createShape(x, y, options) {
   shapes.push(shape);
 }
 
+
+
+
 function draw() {
   background(200);
+  walls.display();
 
-  shapes.forEach( shape => {
-    shape.display();
-  });
-
-  if (mouseIsPressed) {
-    createShape(mouseX, mouseY, null);
+  for (let i = shapes.length-1; i >=0; i--) {
+    const s = shapes[i];
+    s.display();
+    if(s.isDead()) {
+      shapes.splice(i, 1);
+    }
   }
+  
+  /*let targetLeftAngle = PI/4;
+  let targetRightAngle = -PI/4;
+
+  if(keyIsPressed) {
+    if (keyIsDown(LEFT_ARROW)) {
+      targetLeftAngle = -PI/4;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      targetRightAngle = PI/4;
+    }
+  }
+
+  leftPaddle.animAngle(targetLeftAngle);
+  rightPaddle.animAngle(targetRightAngle);*/
 }
